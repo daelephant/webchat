59c52f7bd5cfabd0ced4f38a7e98d01881356ddd
elephant2 <2224168716@qq.com>
2017-04-18 16:02:32 +0800

    Initial commit

---
+++ config/dbconfig.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyx
+ * Date: 2017-04-18
+ * Time: 14:11
+ */
+$dbhost = "127.0.0.1";
+
+$dbuser = "root";
+
+$dbpassword = "hello";
+
+$dbname = "webchat";
+
+//echo "hello!";
+
+
+
